e6d7e7d15139b6080310d7c0b4cc4339d1c7f104
Bajtur Berg <b.berg1@students.htl-leonding.ac.at>
2023-06-13 08:37:24 +0200

    buy button sollte okay sein

--- src/htmlWriter.js
+++ src/htmlWriter.js
@@ -15,7 +15,7 @@ $(() =>{
 
 function renderItems(list, listDiv){
     let html = "<div class=\"container\">";
-    let otherHtml
+    let otherHtml = "<div style=\"display:flex; flex-direction:column\">";
     function renderItem(item){
         html += "<li style='padding-top: 5rem'>"
         html += "<div class=\"menu-card hover:card\">"
@@ -45,10 +45,11 @@ function renderItems(list, listDiv){
         totalCost += parseInt(trimmedPrice, 10)
     }
     html += "</div>"
-    otherHtml = `<span class=\"span title-2\" style="color: hsl(38, 61%, 73%); font-size: xx-large">$${totalCost.toFixed(2)}</span>`
+    otherHtml += `<span class=\"span title-2\" style="color: hsl(38, 61%, 73%); font-size: xx-large">$${totalCost.toFixed(2)}</span>`
     otherHtml += "<button style='content-visibility: visible'><a href=login.html class=\"btn btn-secondary\">" +
         "        <span class=\"text text-1\">Buy Now</span><span class=\"text text-2\" aria-hidden=\"true\">Buy Now</span>" +
         "    </a></button>"
+    otherHtml += "</div>";
     priceDiv.innerHTML = otherHtml
     listDiv.innerHTML = html;
 }
